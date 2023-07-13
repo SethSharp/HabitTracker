02175312633f283087af07d8fa8219153a0ff700
Seth Sharp <sesharp@outlook.com>
2023-07-13 20:22:33 +1000

    wip inertia app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,19 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
-    },
+    title: "Habit Tracker",
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el)
+    },
+    progress: {
+        color: '#c2410c',
+        delay: 0,
     },
 })
