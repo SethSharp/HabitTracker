fb25d9814ddc810bb4fb5ac4000363f097691978
Seth Sharp <sesharp@outlook.com>
2023-07-13 20:15:00 +1000

    vue and init inertia app

--- resources/js/Pages/TestPage.vue
+++ resources/js/Pages/TestPage.vue
@@ -1,14 +1,11 @@
 <script setup>
-import Layout from './Layout'
-import { Head } from '@inertiajs/vue3'
 
-defineProps({ user: Object })
+let user = {
+    name: "Seth"
+}
 </script>
 
 <template>
-    <Layout>
-        <Head title="Welcome" />
-        <h1>Welcome</h1>
-        <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
-    </Layout>
+    <h1>Welcome</h1>
+    <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
 </template>
