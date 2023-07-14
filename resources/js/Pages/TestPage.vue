eb43004173a3d3630cd10091dc09746eeb11b884
Seth Sharp <sesharp@outlook.com>
2023-07-15 07:11:02 +1000

    prettier install plus command

--- resources/js/Pages/TestPage.vue
+++ resources/js/Pages/TestPage.vue
@@ -1,9 +1,9 @@
 <script setup>
-import {BeakerIcon} from "@heroicons/vue/20/solid/index.js";
-    let user = {name:"Seth"}
+import { BeakerIcon } from '@heroicons/vue/20/solid/index.js'
+let user = { name: 'Seth' }
 </script>
 
 <template>
-    <BeakerIcon class="w-10 h-10"/>
-    <h1>Welcome {{ user.name }}, this is your first inertia app </h1>
+  <BeakerIcon class="w-10 h-10" />
+  <h1>Welcome {{ user.name }}, this is your first inertia app</h1>
 </template>
