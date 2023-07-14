c4309acf4753fd2b9118dd6988c6c5d9c8d4ccf3
Seth Sharp <sesharp@outlook.com>
2023-07-15 09:09:08 +1000

    prettier tailwind and linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+	extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "prettier"
+    ],
+    rules: {
+        "vue/component-tags-order": [
+            "error",
+            {
+                order: ["script", "template", "style"],
+            },
+        ],
+        "vue/multi-word-component-names": "off",
+        "vue/component-api-style": ["error", ["script-setup"]],
+        "vue/component-name-in-template-casing": "error",
+        // "vue/block-lang": [
+        //   "error",
+        //   {
+        //     script: { lang: "ts" },
+        //   },
+        // ],
+        "vue/define-macros-order": "error",
+        "vue/no-unused-refs": "error",
+        "vue/no-useless-v-bind": "error",
+        "vue/no-v-html": "off",
+        "vue/require-default-prop": "off",
+        'vue/valid-v-slot': ['error', {
+            allowModifiers: true,
+        }],
+
+        "tailwindcss/no-custom-classname": "off",
+
+        "no-undef": "off",
+        "no-console": "error"
+    },
+    parser: "vue-eslint-parser",
+	parserOptions: {
+        parser: "@typescript-eslint/parser",
+	},
+}
--- resources/js/Pages/TestPage.vue
+++ resources/js/Pages/TestPage.vue
@@ -4,6 +4,7 @@ let user = { name: 'Seth' }
 </script>
 
 <template>
-  <BeakerIcon class="w-10 h-10" />
+  <BeakerIcon
+      class="w-10 h-10" />
   <h1>Welcome {{ user.name }}, this is your first inertia app</h1>
 </template>
