05aef30156c1cd2d151e91644650e52a33611040
Seth Sharp <sesharp@outlook.com>
2023-07-15 14:59:04 +1000

    logo

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,10 +12,8 @@ import { Head } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-red-50">
+                Content
             </div>
         </div>
     </AuthenticatedLayout>
