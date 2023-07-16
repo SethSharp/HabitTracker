873b3fd0afab2b9ceb892715433361c7a9628dfd
Seth Sharp <sesharp@outlook.com>
2023-07-16 14:17:53 +1000

    create habit form

--- app/Http/Controllers/Habits/CreateHabitController.php
+++ app/Http/Controllers/Habits/CreateHabitController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Habits;
 
+use App\Enums\Frequency;
 use App\Http\Controllers\Controller;
-use App\Models\Habit;
 use Inertia\Inertia;
 
 class CreateHabitController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Habits/create');
+        return Inertia::render('Habits/create', [
+            'frequencies' => collect(Frequency::cases())->map(function ($data, $index) {
+                return ['id' => $index, 'name' => $data->name];
+            })
+        ]);
     }
 }
--- resources/js/Pages/Habits/create.vue
+++ resources/js/Pages/Habits/create.vue
@@ -1,7 +1,45 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 import { Head } from '@inertiajs/vue3'
+import {useSchema, FormBuilder, CheckboxGroup} from "@codinglabsau/inertia-form-builder";
+import { Text, Select, Date, PrimaryButton } from "@codinglabsau/ui";
+import {InformationCircleIcon} from "@heroicons/vue/24/outline/index.js";
 
+const props = defineProps({
+    frequencies: Array,
+})
+
+let schema = useSchema({
+    name: Text,
+    description: Text,
+    frequency: {
+        component: Select,
+        props: {
+            options: props.frequencies,
+        },
+    },
+    daily_weekly_configuration: {
+        component: CheckboxGroup,
+        label: 'Days for daily or day for weekly habit',
+        items: [
+            { label: 'Monday', value: 1 },
+            { label: 'Tuesday', value: 2 },
+            { label: 'Wednesday', value: 3 },
+            { label: 'Thursday', value: 4 },
+            { label: 'Friday', value: 5 },
+        ],
+    },
+    monthly_configuration: {
+        component: Date,
+        label: "Select the day for a monthly habit",
+        props: {
+            min: '2023-06-10',
+            max: '2023-08-10'
+        }
+    },
+})
+
+const submit = () => schema.form.post()
 </script>
 
 <template>
@@ -9,7 +47,23 @@ import { Head } from '@inertiajs/vue3'
 
     <AuthenticatedLayout>
         <div class="py-12">
-            This is creating a habit
+            <form onsubmit="submit">
+                <FormBuilder :schema="schema">
+                    <template #actions="{ form }">
+                        <div class="text-gray-400">
+                            <div>
+                                <p>
+<!--                                    <InformationCircleIcon class="w-4 h-4 flex justify-start"/>-->
+                                    Weekly: Only select one day</p>
+                                <p> Daily: Select any day</p>
+                            </div>
+                        </div>
+                        <PrimaryButton>
+                            Create Habit
+                        </PrimaryButton>
+                    </template>
+                </FormBuilder>
+            </form>
         </div>
     </AuthenticatedLayout>
 </template>
