7424c1e9bde7132c5e8d27b2c96a0b054003ae0e
Seth Sharp <sesharp@outlook.com>
2023-07-16 16:16:20 +1000

    convert inertia form builder to normal form

--- resources/js/Pages/Habits/create.vue
+++ resources/js/Pages/Habits/create.vue
@@ -1,8 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import { Head } from '@inertiajs/vue3'
-import {useSchema, FormBuilder, CheckboxGroup} from "@codinglabsau/inertia-form-builder";
-import { Text, Select, Date, PrimaryButton, Container } from "@codinglabsau/ui";
+import { Head, useForm } from '@inertiajs/vue3'
+import {Text, Select, Date, PrimaryButton, Checkbox, Error, Label} from "@codinglabsau/ui";
 
 const props = defineProps({
     frequencies: Array,
@@ -10,61 +9,116 @@ const props = defineProps({
     max: String
 })
 
-let schema = useSchema({
-    name: Text,
-    description: Text,
-    frequency: {
-        component: Select,
-        props: {
-            options: props.frequencies,
-        },
-    },
-    daily_weekly_configuration: {
-        component: CheckboxGroup,
-        label: 'Day/s: Daily, Day: Weekly habits',
-        items: [
-            { label: 'Monday', value: 1 },
-            { label: 'Tuesday', value: 2 },
-            { label: 'Wednesday', value: 3 },
-            { label: 'Thursday', value: 4 },
-            { label: 'Friday', value: 5 },
-        ],
-    },
-    monthly_configuration: {
-        component: Date,
-        label: "Select the day for a monthly habit",
-        props: {
-            min: props.min,
-            max: props.max
-        },
-        description: "This field is only used when making a monthly habit, the same goes for the list of days above (Only being used with daily or weekly habits)"
-    },
-})
+let frequenciesConfig = {
+    options: props.frequencies
+}
 
-const submit = () => schema.form.post(route('habit.store'))
+let weekConfig = {
+    options: [
+        { name: 'Monday', id: 1 },
+        { name: 'Tuesday', id: 2 },
+        { name: 'Wednesday', id: 3 },
+        { name: 'Thursday', id: 4 },
+        { name: 'Friday', id: 5 },
+    ]
+}
+
+const form = useForm({
+    name: '',
+    description: '',
+    frequency: 0,
+    daily_config: [],
+    weekly_config: 0,
+    monthly_config: ''
+});
+
+const submit = () => form.post(route('habit.store'))
 </script>
 
 <template>
     <Head title="Create Habit" />
 
     <AuthenticatedLayout>
-        <Container class="py-12">
+        <div class="bg-gray-200 pl-10">
             <form @submit.prevent="submit">
-                <FormBuilder :schema="schema">
-                    <template #actions="{ form }">
-                        <div class="text-gray-400">
-                            <div>
-                                <p>
-                                    Weekly: Only select one day</p>
-                                <p> Daily: Select any day</p>
-                            </div>
-                        </div>
-                        <PrimaryButton as="button" :loading="form.processing" type="submit">
-                            Create Habit
-                        </PrimaryButton>
-                    </template>
-                </FormBuilder>
+                <div class="w-1/4">
+                    <div class="py-2">
+                        <Label for="name"> Name </Label>
+
+                        <Text
+                            id="name"
+                            ref="name"
+                            v-model="form.name"
+                            class="mt-1 block w-full"
+                        />
+
+                        <Error :error="form.errors.name" class="mt-2" />
+                    </div>
+                    <div class="py-2">
+                        <Label for="description"> Description </Label>
+
+                        <Text
+                            id="description"
+                            ref="description"
+                            v-model="form.description"
+                            class="mt-1 block w-full"
+                        />
+
+                        <Error :error="form.errors.description" class="mt-2" />
+                    </div>
+                    <div class="py-2">
+                        <Label for="frequency"> Frequency </Label>
+
+                        <Select
+                            v-model="form.frequency"
+                            v-bind="frequenciesConfig"
+                            class="mt-1 block w-full"
+                        />
+
+                        <Error :error="form.errors.frequency" class="mt-2" />
+                    </div>
+                    <div class="py-2">
+                        <Label for="daily_config"> Daily </Label>
+
+                        <Checkbox
+                            v-for="(item, index) in weekConfig.options"
+                            :id="item.name ?? item"
+                            :key="index"
+                            v-model="form.daily_config"
+                            :value="item.id ?? item"
+                            :label="item.name ?? item"
+                        />
+
+                        <Error :error="form.errors.daily_config" class="mt-2" />
+                    </div>
+                    <div class="py-2">
+                        <Label for="weekly_config"> Weekly </Label>
+
+                        <Select
+                            v-model="form.weekly_config"
+                            v-bind="weekConfig"
+                            class="mt-1 block w-full"
+                        />
+
+                        <Error :error="form.errors.weekly_config" class="mt-2" />
+                    </div>
+                    <div class="py-2">
+                        <Label for="monthly_config"> Monthly </Label>
+
+                        <Date
+                            v-model="form.monthly_config"
+                            v-model:min="props.min"
+                            v-model:max="props.max"
+                            class="mt-1 block w-full"
+                        />
+
+                        <Error :error="form.errors.monthly_config" class="mt-2" />
+                    </div>
+                </div>
+                <PrimaryButton as="button" :loading="form.processing" type="submit">
+                    Create Habit
+                </PrimaryButton>
             </form>
-        </Container>
+        </div>
     </AuthenticatedLayout>
 </template>
