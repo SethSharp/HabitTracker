887d52035a018852fc64208d6b834b3d8029d814
Seth Sharp <sesharp@outlook.com>
2023-07-17 21:29:52 +1000

    grammar

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -7,19 +7,16 @@
         <div class="w-1/2">
             <h1 class="text-2xl font-bold"> Welcome to, Habit Tracker </h1>
             <div class="leading-loose mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin nisl
-                at purus euismod ultricies. Ut ultrices ante tortor, ac pretium urna tincidunt maximus.
-                Nullam ac tortor id velit lacinia mattis. Sed quis ligula ut velit luctus commodo.
-                Etiam imperdiet volutpat elementum. Vestibulum sit amet lacus dapibus, hendrerit neque at,
-                condimentum elit. Pellentesque accumsan ex ac finibus consectetur. In hac habitasse platea dictumst.
-                Nulla auctor iaculis nunc, id sodales erat dignissim quis. Mauris tristique vel metus eu condimentum.
-                Sed sit amet lobortis ipsum, sed consequat ex.
+                Habit Tracker is a online application you can use to challenge yourself
+                to follow a daily, weekly or monthly routine. Holding yourself accountable
+                to tick off helpful and productive habits. Habit Tracker allows you to set daily
+                habits repeating on any day of the week. Weekly habits on any select day or a monthly
+                habit to remind you at a set date during the month.
             </div>
 
             <h1 class="text-2xl font-bold mt-6"> Future Plans </h1>
             <div class="leading-loose mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin nisl
-                at purus euismod ultricies. Ut ultrices ante tortor, ac pretium urna tincidunt maximus.
+                Friendship system to compete with your friends.... Ut ultrices ante tortor, ac pretium urna tincidunt maximus.
                 Nullam ac tortor id velit lacinia mattis.
             </div>
         </div>
