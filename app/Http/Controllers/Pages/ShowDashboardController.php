4be6629a37a71453bfbd44e61e494cabd6eda9c8
Seth Sharp <sesharp@outlook.com>
2023-07-21 06:25:42 +1000

    start to daily habit card design

--- app/Http/Controllers/Pages/ShowDashboardController.php
+++ app/Http/Controllers/Pages/ShowDashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ShowDashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Dashboard', [
+            // will become a scheduled habit query
+            'habits' => Auth::user()->habits()->get()->toArray(),
+        ]);
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,12 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 import { Head } from '@inertiajs/vue3'
+import Card from "@/Components/Habits/Card.vue"
+import {ChevronDownIcon} from "@heroicons/vue/24/outline/index.js";
+
+const props = defineProps({
+    habits: Array,
+})
 </script>
 
 <template>
@@ -8,7 +14,30 @@ import { Head } from '@inertiajs/vue3'
 
     <AuthenticatedLayout>
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">This is the main area to show stuff</div>
+            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-12">
+                <Card>
+                    <template #heading>
+                        <span class="h-fit py-2 text-2xl"> Today's Habits  </span>
+                    </template>
+                    <template #content>
+                        <div v-if="habits.length > 0">
+                            <div v-for="habit in habits">
+                                <input
+                                    class="w-10 h-10 text-green-500 rounded-full hover:bg-gray-200 focus:ring-green-500"
+                                    type="checkbox"
+                                />
+                                <span class="pl-2 text-xl"> {{ habit.name }} </span>
+                                <button>
+                                    <ChevronDownIcon class="w-8 h-8"/>
+                                </button>
+                            </div>
+                        </div>
+                        <div v-else>
+                            None for today
+                        </div>
+                    </template>
+                </Card>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
