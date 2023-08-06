a692fc1a5e4a6dbfb350761b7fb5b5927a2fad2a
Seth Sharp <sesharp@outlook.com>
2023-08-06 15:39:22 +1000

    more count tests

--- tests/Console/Commands/Counters/HabitStreakTest.php
+++ tests/Console/Commands/Counters/HabitStreakTest.php
@@ -16,6 +16,33 @@ use Tests\Traits\RefreshDatabase;
 class HabitStreakTest extends TestCase
 {
     use RefreshDatabase;
+    
+    /** @test */
+    public function streak_is_reset_to_0_if_not_completed()
+    {
+        Carbon::setTestNow(Carbon::parse("2023-07-17"));
+
+        $user = User::factory()->create();
+
+        Habit::factory()->create([
+            'user_id' => $user->id,
+            'occurrence_days' => '[1]'
+        ]);
+
+        // Setup scheduled habits
+        $this->artisan('habits:schedule-habits')
+            ->assertSuccessful();
+
+        Carbon::setTestNow(Carbon::parse("2023-07-18"));
+
+        $this->artisan('counters:habit-streak')
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('habits', [
+            'user_id' => $user->id,
+            'streak' => 0
+        ]);
+    }
 
     /** @test */
     public function streak_is_stored_if_completed()
@@ -49,14 +76,20 @@ class HabitStreakTest extends TestCase
     }
 
     /** @test */
-    public function streak_is_reset_to_0_if_not_completed()
+    public function multiple_users_habit_streaks_are_stored_if_completed()
     {
         Carbon::setTestNow(Carbon::parse("2023-07-17"));
 
         $user = User::factory()->create();
+        $user2 = User::factory()->create();
 
         Habit::factory()->create([
             'user_id' => $user->id,
+            'occurrence_days' => '[1]'
+        ]);
+
+        Habit::factory()->create([
+            'user_id' => $user2->id,
             'occurrence_days' => '[1]'
         ]);
 
@@ -64,13 +97,24 @@ class HabitStreakTest extends TestCase
         $this->artisan('habits:schedule-habits')
             ->assertSuccessful();
 
+        $scheduledHabits = HabitSchedule::all();
+
+        $scheduledHabits[0]->update(['completed' => 1]);
+
         Carbon::setTestNow(Carbon::parse("2023-07-18"));
 
         $this->artisan('counters:habit-streak')
             ->assertSuccessful();
 
         $this->assertDatabaseHas('habits', [
+            'id' => $scheduledHabits[0]->id,
             'user_id' => $user->id,
+            'streak' => 1
+        ]);
+
+        $this->assertDatabaseHas('habits', [
+            'id' => $scheduledHabits[1]->id,
+            'user_id' => $user2->id,
             'streak' => 0
         ]);
     }
