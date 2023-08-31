686f0dcdfc20ec4972a5d57218d2945f5b150f5a
Seth Sharp <sesharp@outlook.com>
2023-08-31 14:34:15 +1000

    improve scheduling habit command test

--- tests/Console/Commands/ScheduledHabits/ScheduleHabitsForMonthTest.php
+++ tests/Console/Commands/ScheduledHabits/ScheduleHabitsForMonthTest.php
@@ -34,6 +34,31 @@ class ScheduleHabitsForMonthTest extends TestCase
 
         // 5 weeks, where there is a tue/wed/thur
         $this->assertCount(15, $habitSchedules);
+
+        // Week 1
+        $this->assertEquals("2023-08-01", $habitSchedules[0]->scheduled_completion);
+        $this->assertEquals("2023-08-02", $habitSchedules[1]->scheduled_completion);
+        $this->assertEquals("2023-08-03", $habitSchedules[2]->scheduled_completion);
+
+        // Week 2
+        $this->assertEquals("2023-08-08", $habitSchedules[3]->scheduled_completion);
+        $this->assertEquals("2023-08-09", $habitSchedules[4]->scheduled_completion);
+        $this->assertEquals("2023-08-10", $habitSchedules[5]->scheduled_completion);
+
+        // Week 3
+        $this->assertEquals("2023-08-15", $habitSchedules[6]->scheduled_completion);
+        $this->assertEquals("2023-08-16", $habitSchedules[7]->scheduled_completion);
+        $this->assertEquals("2023-08-17", $habitSchedules[8]->scheduled_completion);
+
+        // Week 4
+        $this->assertEquals("2023-08-22", $habitSchedules[9]->scheduled_completion);
+        $this->assertEquals("2023-08-23", $habitSchedules[10]->scheduled_completion);
+        $this->assertEquals("2023-08-24", $habitSchedules[11]->scheduled_completion);
+
+        // Week 5
+        $this->assertEquals("2023-08-29", $habitSchedules[12]->scheduled_completion);
+        $this->assertEquals("2023-08-30", $habitSchedules[13]->scheduled_completion);
+        $this->assertEquals("2023-08-31", $habitSchedules[14]->scheduled_completion);
     }
 
     /** @test */
@@ -55,6 +80,12 @@ class ScheduleHabitsForMonthTest extends TestCase
         $habitSchedules = HabitSchedule::all();
 
         $this->assertCount(5, $habitSchedules);
+
+        $this->assertEquals("2023-08-02", $habitSchedules[0]->scheduled_completion);
+        $this->assertEquals("2023-08-09", $habitSchedules[1]->scheduled_completion);
+        $this->assertEquals("2023-08-16", $habitSchedules[2]->scheduled_completion);
+        $this->assertEquals("2023-08-23", $habitSchedules[3]->scheduled_completion);
+        $this->assertEquals("2023-08-30", $habitSchedules[4]->scheduled_completion);
     }
 
     /** @test */
