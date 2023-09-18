902f9a9606b8ac2577d48287667f15f57bd7a241
Seth Sharp <sesharp@outlook.com>
2023-09-18 18:19:47 +1000

    icons, composer and publish

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -4,9 +4,9 @@ import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/24/outline/index.js
 
 <template>
     <div class="grid grid-cols-2">
-        <XCircleIcon class="w-8 h-8" />
-        <CheckCircleIcon class="w-8 h-8" />
-        <CheckCircleIcon class="w-8 h-8" />
-        <CheckCircleIcon class="w-8 h-8" />
+        <XCircleIcon class="w-8 h-8 text-red-500" />
+        <CheckCircleIcon class="w-8 h-8 text-green-500" />
+        <CheckCircleIcon class="w-8 h-8 text-green-500" />
+        <CheckCircleIcon class="w-8 h-8 text-green-500" />
     </div>
 </template>
