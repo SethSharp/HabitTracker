777e4b70a39534f7bd1a5bdea6e2e75f495f96bf
Seth Sharp <sesharp@outlook.com>
2023-10-10 19:54:11 +1000

    production .env.example and copy db data over

--- app/Console/Commands/ScheduledHabits/ScheduleHabitsForMonth.php
+++ app/Console/Commands/ScheduledHabits/ScheduleHabitsForMonth.php
@@ -15,6 +15,7 @@ class ScheduleHabitsForMonth extends Command
     use ScheduledHabits;
     use HabitStorage;
 
+    // TODO: Needs to re-use current logic for scheduling a habit
     protected $signature = 'habits:schedule-habits';
     protected $description = 'Set habits up for the month based on the occurrence days of the habit';
 
@@ -30,26 +31,26 @@ class ScheduleHabitsForMonth extends Command
             $habits = $user->habits()->get();
 
             $habits->map(function ($habit) use ($user, $startDate, $endDate) {
-                if ($habit['frequency']->value == Frequency::MONTHLY->value) {
-                    HabitSchedule::create([
-                        'habit_id' => $habit->id,
-                        'user_id' => $user->id,
-                        'scheduled_completion' => json_decode($habit['occurrence_days'])[0],
-                    ]);
-                } else {
-                    $occurrences = json_decode($habit->occurrence_days);
-
-                    while ($startDate <= $endDate) {
-                        if (in_array($startDate->dayOfWeek, $occurrences)) {
-                            HabitSchedule::create([
-                                'habit_id' => $habit->id,
-                                'user_id' => $user->id,
-                                'scheduled_completion' => $startDate
-                            ]);
-                        }
-                        $startDate->addDay();
-                    }
-                }
+//                if ($habit['frequency']->value == Frequency::MONTHLY->value) {
+//                    HabitSchedule::create([
+//                        'habit_id' => $habit->id,
+//                        'user_id' => $user->id,
+//                        'scheduled_completion' => json_decode($habit['occurrence_days'])[0],
+//                    ]);
+//                } else {
+//                    $occurrences = json_decode($habit->occurrence_days);
+//
+//                    while ($startDate <= $endDate) {
+//                        if (in_array($startDate->dayOfWeek, $occurrences)) {
+//                            HabitSchedule::create([
+//                                'habit_id' => $habit->id,
+//                                'user_id' => $user->id,
+//                                'scheduled_completion' => $startDate
+//                            ]);
+//                        }
+//                        $startDate->addDay();
+//                    }
+//                }
             });
         });
     }
