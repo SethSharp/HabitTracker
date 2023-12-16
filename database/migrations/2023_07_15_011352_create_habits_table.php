50fdf2eb5e7762e04ad02791e6b66bc1ed7d5eb6
Seth Sharp <sesharp@outlook.com>
2023-12-16 14:37:02 +1000

    run migrations automatically

--- database/migrations/2023_07_15_011352_create_habits_table.php
+++ database/migrations/2023_07_15_011352_create_habits_table.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class() extends Migration {
+return new class() extends Migration
+{
     public function up(): void
     {
         Schema::create('habits', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->string('description');
             $table->string('frequency');
             $table->string('scheduled_to')->nullable();
             $table->json('occurrence_days')->nullable();
